18b4b4fc7932336f815e32cdd25e77136f7c1d24
Codrut GUSOI <codrut.gusoi@zitec.ro>
2014-07-17 17:48:57 +0300

    Updated links

--- remote_install.sh
+++ remote_install.sh
@@ -2,14 +2,14 @@
 
 #create the .git_hooks folder in the user's home, downloads the git-hooks script and makes it executable
 mkdir ~/.githook
-wget -P ~/.githook https://raw.githubusercontent.com/SDW-CC/git-hooks/master/githook
-wget -P ~/.githook https://raw.githubusercontent.com/SDW-CC/git-hooks/master/LICENSE
-wget -P ~/.githook https://github.com/SDW-CC/git-hooks/blob/master/README.md
+wget -P ~/.githook https://raw.githubusercontent.com/SDWolf/git-hooks/master/githook
+wget -P ~/.githook https://raw.githubusercontent.com/SDWolf/git-hooks/master/LICENSE
+wget -P ~/.githook https://github.com/SDWolf/git-hooks/blob/master/README.md
 chmod +x ~/.githook/githook
 
 #creates the commit-msg folder and downloads the commit-msg hook script
 mkdir ~/.githook/commit-msg
-wget -P ~/.githook/commit-msg https://raw.githubusercontent.com/SDW-CC/git-hooks/master/hooks/commit-msg/25characterlimit.sh
+wget -P ~/.githook/commit-msg https://raw.githubusercontent.com/SDWolf/git-hooks/master/hooks/commit-msg/25characterlimit.sh
 chmod +x ~/.githook/commit-msg/25characterlimit.sh
 
 echo "export PATH=\$PATH:~/.githook" >> ~/.bash_profile
